a70762a81f5f5a314eeebd64d28f3beda32f06c5
Brandon1513 <brandon.devora.lucio@gmail.com>
2025-05-07 13:54:34 -0600

    nuevos cambios al back server

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const qrRoutes = require("./routes/qrRoutes");
-/*PDF*/
 const pdfRoutes = require("./routes/pdfRoutes");
 const path = require("path");
-
-//Recetas
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
@@ -17,26 +14,31 @@ const PORT = process.env.PORT || 5000;
 // Conectar a la base de datos
 connectDB();
 
+// ✅ CORS configurado para frontend local y productivo
+app.use(cors({
+  origin: [
+    "http://localhost:5173",              // desarrollo local
+    "https://recetasdasa.domcloud.dev"    // producción
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
 
-/*PDF*/
+// PDF
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Ruta User
+// Rutas
 app.use("/user", require("./routes/userRoutes"));
-// Ruta QR
 app.use("/qr", qrRoutes);
-
-//Rutas PDF
 app.use("/api", pdfRoutes);
-
-//Ruta Recetas
 app.use("/products", productRoutes);
 
+// Inicializar servidor
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
